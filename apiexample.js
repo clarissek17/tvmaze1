4e5dcd9396b69eb068c64bc34c6e89c9a524d43b
clarissek17 <cyan12.ky@gmail.com>
2024-12-19 10:12:36 -0800

    Add files via upload

--- apiexample.js
+++ apiexample.js
@@ -4,7 +4,7 @@ let message;
 // load the service worker
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+    navigator.serviceWorker.register('tvmaze1/sw.js').then(function(registration) {
       console.log('Service Worker registered with scope:', registration.scope);
     }, function(error) {
       console.log('Service Worker registration failed:', error);
